9e6687b30adb68f48eefed85d5561a112c9f99dd
areiahna <areiahnacooks@yahoo.com>
2024-05-30 02:00:06 -0400

    Task2 complete

--- adventure_game/task2.py
+++ adventure_game/task2.py
@@ -1,4 +1,4 @@
-# Task 2: Setting the Scene
+# Task 2: Setting the Scene..
 
 #Based on the corrected code from Task 1, expand the game. If the user chooses "cave", instead of printing "You find a hidden treasure!" ask them if they want to "light a torch" or "proceed in the dark", and provide outcomes for each decision.
 
