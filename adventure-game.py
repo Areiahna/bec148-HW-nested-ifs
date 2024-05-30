303ff4ac4c0625c57761a9ee2357741f1331e52b
areiahna <areiahnacooks@yahoo.com>
2024-05-30 01:47:34 -0400

    Nested Decisions - Task1

---
+++ adventure-game.py
@@ -0,0 +1,19 @@
+# Task 1: Code Correction
+
+# You are provided with a Python script that is supposed to guide a user through an adventure game, but it has some errors. Identify and fix them
+
+place = input("Choose a place: forest or cave? ")
+
+if (place == "forest"):
+        action = input("climb a tree or cross a river?")
+        if (action == "climb a tree"):
+            print("You found a bird's nest!")
+        else:
+            action = "cross a river"
+            print("You found a boat!")
+else:
+    place = "cave"
+    print("You find a hidden treasure!")
+
+
+print("="*50)
